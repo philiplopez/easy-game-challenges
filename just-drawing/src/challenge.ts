0efb292a73acc5d87b17ee1e21d47efdb54c1f90
Philip Lopez <philip.lopez@philiplopez.com>
2017-04-06 15:59:14 +1000

    bumped easy-draw, some style

--- just-drawing/src/challenge.ts
+++ just-drawing/src/challenge.ts
@@ -4,9 +4,14 @@ import {makeRender} from "easy-draw/lib/canvasRenderer"
 // D.circle({centre x & y}, radius)
 // D.rectangle({topLeft x & y}, {dimensions in x & y})
 const graphics = [
-    D.circle({x: 120, y: 120}, 100),
-  
-    D.rectangle({x: 90, y:90 }, {x: 15, y: 15}),
+    D.circle({x: 120, y: 120}, 100, {
+        fillColour: D.rgba(80, 0, 80, 0.5),
+        strokeColour: D.rgba(80, 20, 255, 0.8)
+    }),
+    D.rectangle({x: 90, y:90 }, {x: 15, y: 15}, {
+        fillColour: D.rgba(50, 0, 50, 0.5),
+        strokeColour: D.rgba(80, 20, 255, 0.8)
+    }),
     D.rectangle({x: 80, y:80 }, {x: 15, y: 15}),
     D.rectangle({x: 100, y:100 }, {x: 15, y: 15}),
     D.rectangle({x: 110, y:110 }, {x: 15, y: 15}),
@@ -21,7 +26,6 @@ const graphics = [
     D.rectangle({x: 150, y:150 }, {x: 15, y: 15}),
 ];
 
-
-const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const renderToScreen = makeRender(canvas.getContext("2d"), true);
 renderToScreen(graphics)
