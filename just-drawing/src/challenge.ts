7021bd96942646cbfeceb04fed3dd73d59be9188
Philip Lopez <philip.lopez@philiplopez.com>
2017-04-10 18:39:35 +1000

    bumped easy-draw to 0.6.0 with named args

--- just-drawing/src/challenge.ts
+++ just-drawing/src/challenge.ts
@@ -1,31 +1,34 @@
 import * as D from "easy-draw/lib/namedAPI"
 import {makeRender} from "easy-draw/lib/canvasRenderer"
 
-// D.circle({centre x & y}, radius)
-// D.rectangle({topLeft x & y}, {dimensions in x & y})
 const graphics = [
-    D.circle({x: 120, y: 120}, 100, {
-        fillColour: D.rgba(80, 0, 80, 0.5),
-        strokeColour: D.rgba(80, 20, 255, 0.8)
+    D.circle({
+        centre_px: {
+            x: 120,
+            y: 120
+        },
+        radius_px: 100,
+        style: {
+            fillRGBA: D.rgba(80, 0, 80, 0.5),
+            strokeRGBA: D.rgba(80, 20, 255, 0.8)
+        }
     }),
-    D.rectangle({x: 90, y:90 }, {x: 15, y: 15}, {
-        fillColour: D.rgba(50, 0, 50, 0.5),
-        strokeColour: D.rgba(80, 20, 255, 0.8)
-    }),
-    D.rectangle({x: 80, y:80 }, {x: 15, y: 15}),
-    D.rectangle({x: 100, y:100 }, {x: 15, y: 15}),
-    D.rectangle({x: 110, y:110 }, {x: 15, y: 15}),
-    D.rectangle({x:50, y:50 }, {x: 15, y: 15}),
-    D.rectangle({x: 120, y:120 }, {x: 15, y: 15}),
-    D.rectangle({x: 70, y:70 }, {x: 15, y: 15}),
-    D.rectangle({x: 60, y:60 }, {x: 15, y: 15}),
-    D.rectangle({x: 40, y:40 }, {x: 15, y: 15}),
-    D.rectangle({x: 130, y:130 }, {x: 15, y: 15}),
-    D.rectangle({x: 140, y:140 }, {x: 15, y: 15}),
-    D.rectangle({x: 30, y:30 }, {x: 15, y: 15}),
-    D.rectangle({x: 150, y:150 }, {x: 15, y: 15}),
+    D.rectangle({
+        topLeft_px: {
+            x: 90,
+            y: 90
+        },
+        dimensions_px: {
+            x: 15,
+            y: 15
+        },
+        style: {
+            fillRGBA: D.rgba(50, 0, 50, 0.792),
+            strokeRGBA: D.rgba(80, 20, 255, 0.8)
+        }
+    })
 ];
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const canvas = document.getElementById("canvas")as HTMLCanvasElement;
 const renderToScreen = makeRender(canvas.getContext("2d"), true);
 renderToScreen(graphics)
